3e9e04fe311ee4d2a23589173756037d4527779c
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2020-12-24 11:05:01 -0300

    feat(postgresdb): added postgres repository with typeorm

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   plugins: ["@typescript-eslint", "prettier"],
   rules: {
     "prettier/prettier": "error",
+    "class-methods-use-this": "off",
     "import/extensions": [
       "error",
       "ignorePackages",
