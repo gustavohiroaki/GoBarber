90c54fbcfc31788a6df2a3d24ee9a792702194aa
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2020-12-27 23:22:36 -0300

    feat(user): user create funcion added

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   rules: {
     "prettier/prettier": "error",
     "class-methods-use-this": "off",
+    camelcase: "off",
     "import/extensions": [
       "error",
       "ignorePackages",
