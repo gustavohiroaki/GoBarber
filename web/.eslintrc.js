6b433eda8028c33cd671db8538e06ddb2353fea8
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2021-01-11 16:02:33 -0300

    feat(web): basic structure

---
+++ web/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "react-hooks", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+      },
+    ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
+    "import/prefer-default-export": "off",
+    camelcase: "off",
+    "@typescript-eslint/ban-types": "off",
+    "no-use-before-define": "off",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
