b790827431d8c9a39a24a2dbf9d9c360b4a1834c
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2021-01-12 23:27:35 -0300

    style(signin): signin stylized

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
+import SignIn from './pages/Signin';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <SignIn />
+      <GlobalStyle />
+    </>
   );
 }
 
