ea889899abf5d9a8bffbeb758453a3fa2789cc4a
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2021-01-22 17:30:44 -0300

    test(appointments): created sample test for CreateAppointmentService

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,21 @@
-test("sum two numbers", () => {
-  const sum = 1 + 3;
+import FakeAppointmentsRepository from "@modules/appointments/repositories/fakes/FakeAppointmentsRepository";
+import CreateAppointmentService from "@modules/appointments/services/CreateAppointmentService";
 
-  expect(sum).toBe(4);
+describe("CreateAppointment", () => {
+  it("should be able to create a new appointment", async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: "123123123",
+    });
+
+    expect(appointment).toHaveProperty("id");
+    expect(appointment.provider_id).toBe("123123123");
+  });
+  // it("should not be able to create two appointment on the same time", () => {});
 });
