b46f161c43836da0d2484276987a4d27b21065db
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2021-01-23 10:33:26 -0300

    test(appointments): tests 100% coverage

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,6 +1,8 @@
 import FakeAppointmentsRepository from "@modules/appointments/repositories/fakes/FakeAppointmentsRepository";
 import CreateAppointmentService from "@modules/appointments/services/CreateAppointmentService";
 
+import AppError from "@shared/errors/AppError";
+
 describe("CreateAppointment", () => {
   it("should be able to create a new appointment", async () => {
     const fakeAppointmentsRepository = new FakeAppointmentsRepository();
@@ -17,5 +19,25 @@ describe("CreateAppointment", () => {
     expect(appointment).toHaveProperty("id");
     expect(appointment.provider_id).toBe("123123123");
   });
-  // it("should not be able to create two appointment on the same time", () => {});
+  it("should not be able to create two appointment on the same time", async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+
+    const date = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date,
+      provider_id: "123123123",
+    });
+
+    expect(
+      createAppointment.execute({
+        date,
+        provider_id: "123123123",
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
