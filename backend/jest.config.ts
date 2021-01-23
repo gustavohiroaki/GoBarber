98ce3e66fbd75d41dba7dc1b6c66cba00b7a1755
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2021-01-23 10:25:29 -0300

    test(jest): coverage reports

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -6,6 +6,10 @@ const { compilerOptions } = require("./tsconfig.json");
 export default {
   clearMocks: true,
 
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/modules/**/services/*.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text-summary", "lcov"],
   coverageProvider: "v8",
 
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
