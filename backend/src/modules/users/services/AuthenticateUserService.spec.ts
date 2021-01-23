d47bde7fe956be6b73b48eeb69b220f013016123
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2021-01-23 13:49:12 -0300

    test(authenticateservice): tests 100% coverage

--- backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -33,4 +33,49 @@ describe("AuthenticateUser", () => {
 
     expect(response).toHaveProperty("token");
   });
+
+  it("should not be able to authenticate with non existing user", async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: "johndsdoe@example.com",
+        password: "123123123",
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should be not able to authenticate with wrong password", async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    await createUser.execute({
+      name: "John Doe 2",
+      email: "johndoe2@example.com",
+      password: "1231231asd",
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: "johndoe2@example.com",
+        password: "1231231d23",
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
