818c3c8307059e9dbdd729611c7639f127f6dfee
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2021-01-26 22:41:47 -0300

    feat(mail handlebars): added handlebars and handling variables

--- backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -33,9 +33,19 @@ export default class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
-    await this.mailProvider.sendMail(
-      "test@test.com",
-      `Pedido de recuperação de senha recebido: ${token}`
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: "[GoBarber] Recuperação de Senha",
+      templateData: {
+        template: "Olá, {{name}}:{{token}}",
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
--- backend/src/shared/providers/index.ts
+++ backend/src/shared/providers/index.ts
@@ -4,14 +4,22 @@ import IStorageProvider from "@shared/providers/StorageProvider/models/IStorageP
 import DiskStorageProvider from "@shared/providers/StorageProvider/implementations/DiskStorageProvider";
 
 import IMailProvider from "@shared/providers/MailProvider/models/IMailProvider";
-import EtherealMailProvider from "@shared/providers/MailProvider/implementation/EtherealMailProvider";
+import EtherealMailProvider from "@shared/providers/MailProvider/implementations/EtherealMailProvider";
+
+import IMailTemplateProvider from "@shared/providers/MailTemplateProvider/models/IMailTemplateProvider";
+import HandlebarsMailTemplateProvider from "@shared/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider";
 
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
   DiskStorageProvider
 );
 
+container.registerSingleton<IMailTemplateProvider>(
+  "MailTemplateProvider",
+  HandlebarsMailTemplateProvider
+);
+
 container.registerInstance<IMailProvider>(
   "MailProvider",
-  new EtherealMailProvider()
+  container.resolve(EtherealMailProvider)
 );
