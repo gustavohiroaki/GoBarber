9d8f29c34b6c4533d1ac98ecc0dc3517849da991
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2021-02-01 15:16:24 -0300

    perf(cache): invalidating caches when register user

--- backend/src/modules/users/services/CreateUserService.ts
+++ backend/src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 
 import IUsersRepository from "@modules/users/repositories/IUsersRepository";
 import IHashProvider from "@modules/users/providers/HashProvider/models/IHashProvider";
+import ICacheProvider from "@shared/providers/CacheProvider/models/ICacheProvider";
 
 import AppError from "@shared/errors/AppError";
 
@@ -20,7 +21,10 @@ export default class CreateUserService {
     private usersRepository: IUsersRepository,
 
     @inject("HashProvider")
-    private hashProvider: IHashProvider
+    private hashProvider: IHashProvider,
+
+    @inject("CacheProvider")
+    private cacheProvider: ICacheProvider
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -37,6 +41,8 @@ export default class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix("providers-list");
 
     await this.usersRepository.save(user);
 
