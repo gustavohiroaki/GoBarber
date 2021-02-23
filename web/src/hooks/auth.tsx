3f60f6f4ef1272535b0736945fc1e6ec944d92da
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2021-02-23 16:26:06 -0300

    test(hooks): auth hooks test

--- web/src/hooks/auth.tsx
+++ web/src/hooks/auth.tsx
@@ -84,9 +84,5 @@ export const AuthProvider: React.FC = ({ children }) => {
 export function useAuth(): IAuthContext {
   const context = useContext(AuthContext);
 
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-
   return context;
 }
